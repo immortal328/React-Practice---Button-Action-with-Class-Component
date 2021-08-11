904ef6bffb242bb4a802b1c8156b424e18608491
Amar H. Pujari <47078596+immortal328@users.noreply.github.com>
2021-08-11 20:41:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,18 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	    this.state ={para:""};
 	};
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+			<button id="click" 
+				onClick={() => {
+            			this.setState({ para:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy" });
+          			}}></button>
+		
+			<p id="para">(this.state.para)</p>
     		</div>
     	);
     }
