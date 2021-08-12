331882442f97f995525d43f97a56965a27e3f28c
Amar H. Pujari <47078596+immortal328@users.noreply.github.com>
2021-08-13 00:23:36 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,19 @@ import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-	super(props);
-	this.state ={text:false};
-	this.txt ="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-    };
+		super(props);
+	    this.state ={text:false};
+		this.txt ="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+	};
 
     render() {
     	return(
     		<div id="main">
-			<button id="click"
-			onClick={function(){
-				this.state ={text:!text}
-			}}></button>
-			{this.state.text? (<h1 id="para">{this.txt}</h1>):null}
+				<button id="click" 
+				onClick={()=>{
+				this.setState({text:!this.state.text})
+				}}>Toggle</button>
+				{this.state.text? (<h1 id="para">{this.txt}</h1>):null}
     		</div>
     	);
     }
